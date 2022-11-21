b1efe786011c9738e8ed72411a24f0457e7cc637
lujolfs <lucas.j.frank@gmail.com>
2022-11-21 13:49:08 -0300

    Implementação de sign-up e sign-in via front

--- src/sign-in/Signin.js
+++ src/sign-in/Signin.js
@@ -1,15 +1,52 @@
+import { useState } from "react";
 import styled from "styled-components";
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import { Button, Container } from "../assets/styled-components/Styled";
 import Logo from "../assets/styled-components/Logo"
+import axios from "axios";
 
 export default function Signin() {
+    const [form, setForm] = useState({
+        email: '',
+        password: '',
+    });
+
+    const navigate = useNavigate();
+    const [disabled, setDisabled] = useState(false);
+
+    function logUser(event) {
+        const envio = axios.post("http://localhost:5000/sign-in", form)
+        event.preventDefault();
+        setDisabled(true);
+        envio.then(checkPost);
+        envio.catch(checkError);
+    };
+
+    function handleForm(e) {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    function checkPost() {
+        navigate("/");
+    }
+
+    function checkError() {
+        setDisabled(false);
+        setForm({
+            email: '',
+            password: '',
+        })
+    }
+
     return (
         <Container>
             <Logo/>
-            <Form>
-                <Field type="text" name="email" placeholder="E-mail"/>
-                <Field type="password" name="password" placeholder="Senha"/>
+            <Form onSubmit={logUser}>
+                <Field type="text" name="email" placeholder="E-mail" value={form.email} onChange={handleForm} disabled={disabled}/>
+                <Field type="password" name="password" placeholder="Senha" value={form.password} onChange={handleForm} disabled={disabled}/>
                 <Button>Entrar</Button>
             </Form>
             <StyledLink to={`/sign-up`}>
--- src/sign-up/Signup.js
+++ src/sign-up/Signup.js
@@ -1,21 +1,64 @@
+import { useState } from "react";
 import styled from "styled-components";
-import {Link} from 'react-router-dom'
+import { Link, useNavigate } from "react-router-dom";
 import { Button, Container } from "../assets/styled-components/Styled";
 import Logo from "../assets/styled-components/Logo"
+import axios from "axios";
 
 export default function Signup() {
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        password: '',
+        repeat_password: ''
+    });
+
+    const navigate = useNavigate();
+
+    const [disabled, setDisabled] = useState(false);
+
+    function createUser(event) {
+        const envio = axios.post("http://localhost:5000/sign-up", form)
+        event.preventDefault();
+        setDisabled(true);
+        envio.then(checkPost);
+        envio.catch(checkError);
+    };
+
+    function handleForm(e) {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    function checkPost() {
+        alert("Conta criada com sucesso!");
+        navigate("/sign-in");
+    }
+
+    function checkError() {
+        setDisabled(false);
+        setForm({
+            name: '',
+            email: '',
+            password: '',
+            repeat_password: ''
+        })
+    }
+
     return (
         <Container>
-            <Logo/>
-            <Form>
-                <Field type="text" name="name" placeholder="Nome"/>
-                <Field type="text" name="email" placeholder="E-mail"/>
-                <Field type="password" name="password" placeholder="Senha"/>
-                <Field type="password" name="password" placeholder="Confirme a senha"/>
+            <Logo />
+            <Form onSubmit={createUser}>
+                <Field type="text" name="name" placeholder="Nome" value={form.name} onChange={handleForm} disabled={disabled} />
+                <Field type="text" name="email" placeholder="E-mail" value={form.email} onChange={handleForm} disabled={disabled} />
+                <Field type="password" name="password" placeholder="Senha" value={form.password} onChange={handleForm} disabled={disabled} />
+                <Field type="password" name="repeat_password" placeholder="Confirme a senha" value={form.repeat_password} onChange={handleForm} disabled={disabled} />
                 <Button>Cadastrar</Button>
             </Form>
             <StyledLink to={`/sign-in`}>
-                    Já tem uma conta? Entre agora!
+                Já tem uma conta? Entre agora!
             </StyledLink>
         </Container>
     )
@@ -36,6 +79,7 @@ border-radius: 5px;
 height: 60px;
 width: 325px;
 padding-left: 15px;
+font-family: 'Raleway', sans-serif;
 `
 
 const StyledLink = styled(Link)`
